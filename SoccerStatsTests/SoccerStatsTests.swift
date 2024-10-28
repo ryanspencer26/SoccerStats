f630a8fc9d9faaf446321b4d22e7e6a1ae6eac14
Ryan Spencer <ryan.spencer26@d155.org>
2024-10-28 08:49:29 -0500

    Initial Commit

---
+++ SoccerStatsTests/SoccerStatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerStatsTests.swift
+//  SoccerStatsTests
+//
+//  Created by RYAN SPENCER on 10/28/24.
+//
+
+import Testing
+@testable import SoccerStats
+
+struct SoccerStatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
