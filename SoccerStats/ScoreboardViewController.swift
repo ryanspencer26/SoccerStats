3d67de1b614f7ddaf8ea47aa9d0f3d06f4f440b7
Ryan Spencer <ryan.spencer26@d155.org>
2024-10-31 08:08:44 -0500

    still working on UI

--- SoccerStats/ScoreboardViewController.swift
+++ SoccerStats/ScoreboardViewController.swift
@@ -7,23 +7,23 @@
 
 import UIKit
 
+class AppData: Codable{
+    static var time = 90.0
+}
+
 class ScoreboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func startTimer(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func pauseTimer(_ sender: Any) {
+    }
+    
 }
